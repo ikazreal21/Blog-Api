eb63c482969f6f236a8abcf3b65c16c193801d73
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-09-21 12:13:03 +0800

    Post and Comments Models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=255)
+    body = models.TextField(blank=True)
+    ranid = models.CharField(
+        max_length=100, default=uuid.uuid4, editable=False, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.title | str(self.user)
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    comment = models.text_field(blank=True)
