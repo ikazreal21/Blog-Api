680e2cc3fe236defcbcbfb63b923109b2c69450e
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-09-21 12:19:56 +0800

    Migrate Models

--- blog/models.py
+++ blog/models.py
@@ -3,18 +3,27 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Post(models.Model):
+class Comments(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    title = models.CharField(max_length=255)
-    body = models.TextField(blank=True)
+    comment = models.TextField(blank=True)
     ranid = models.CharField(
         max_length=100, default=uuid.uuid4, editable=False, null=True, blank=True
     )
 
     def __str__(self):
-        return self.title | str(self.user)
+        return str(self.user)
 
 
-class Comments(models.Model):
+class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    comment = models.text_field(blank=True)
+    title = models.CharField(max_length=255)
+    body = models.TextField(blank=True)
+    comment = models.ForeignKey(
+        Comments, on_delete=models.CASCADE, blank=True, null=True
+    )
+    ranid = models.CharField(
+        max_length=100, default=uuid.uuid4, editable=False, null=True, blank=True
+    )
+
+    def __str__(self):
+        return self.title + "|" + str(self.user)
