2b4d7998b8362ab6aecc078917f65f894e499e10
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-09-29 12:35:56 +0800

    Added Some Permissions

--- Blog_Api/settings.py
+++ Blog_Api/settings.py
@@ -118,6 +118,13 @@ USE_L10N = True
 USE_TZ = True
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly'
+    ]
+}
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
