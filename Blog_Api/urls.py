7d3e047e710cd9febd4eee849bdb77c452502eee
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-10-23 00:18:33 +0800

    Added the Rest URLS for Login

--- Blog_Api/urls.py
+++ Blog_Api/urls.py
@@ -5,4 +5,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls'), name='post'),
+    # API AUTH
+    path("api-auth/", include('rest_framework.urls'), name='rest_framework'),
 ]
