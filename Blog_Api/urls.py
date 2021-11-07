171c4d826287e95bf913256828c947cc4fa3d2b7
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-11-08 00:05:02 +0800

    Added the users enpoint

--- Blog_Api/urls.py
+++ Blog_Api/urls.py
@@ -7,7 +7,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('blog.urls'), name='post'),
     # API AUTH
-    path("api-auth/", include('rest_framework.urls'), name='rest_framework'),
+    path('api-auth/', include('rest_framework.urls'), name='rest_framework'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # API REGISTER User
+    path('api/users/', include('users.urls'), name='users'),
 ]
