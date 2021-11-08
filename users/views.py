38b0e4e8d0a78863f4ed9c28197e55aec1622058
ikazreal21 <66871228+ikazreal21@users.noreply.github.com>
2021-11-09 00:02:59 +0800

    Added the BlacklistTokenView

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import RegisterUserSerializer
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 class CreateUser(APIView):
@@ -15,3 +16,15 @@ class CreateUser(APIView):
             if newuser:
                 return Response(status=status.HTTP_201_CREATED)
         return Response(reg_serial.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BlacklistTokenView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        try:
+            refesh = request.data['refresh_token']
+            token = RefreshToken(refresh)
+            token.blacklist()
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
